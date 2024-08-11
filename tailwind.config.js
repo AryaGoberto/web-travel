a15bd014983018fadddab71a4284defe00616f3a
AryaGoberto <arguna2019@gmail.com>
2024-08-11 20:30:38 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#5D50C6",
+        pink: "#F85E9F",
+        black: "#191825",
+        orange: "#FF5722",
+        kuning: "#FFFBF1",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
